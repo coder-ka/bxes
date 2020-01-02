7fdc597493626dbd46a172fa88e2eb845061a450
katsu <libeera.k@gmail.com>
2020-01-03 03:20:53 +0900

    export library interace

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Box from "./components/Box";
+import Boundary from "./components/Boundary";
+import Connector from "./components/Connector";
+import Connection from "./components/Connection";
+
+export const Box = Box;
+export const Boundary = Boundary;
+export const Connector = Connector;
+export const Connection = Connection;
